b386a356d45569609ec0a643c376313621d078a7
sodervan <egbowonemmanuel99@gmail.com>
2025-01-23 00:34:20 +0100

    edited the body a little

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -6,6 +6,7 @@ import Body from "@/components/Body";
 import Footer from "@/components/Footer";
 import ContactSection from "@/components/HomeComponents/ContactSection";
 import ContinuousTestimonials from "@/components/HomeComponents/ContinuousTestimonials";
+import UserRolesShowcase from "@/components/HomeComponents/UserRolesShowcase";
 // import { useOutletContext } from "react-router-dom";
 
 const HomePage = () => {
@@ -22,6 +23,7 @@ const HomePage = () => {
     <>
       <Body userId={id} />
       <div className="h-40 "></div>
+      <UserRolesShowcase />
       <WhoWeAre />
       <ContinuousTestimonials />
       <General />

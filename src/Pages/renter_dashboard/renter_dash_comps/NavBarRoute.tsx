058c1049c7cc517947737a9fdf6e85886fb04269
destiny <desto4q@gmail.com>
2025-02-25 20:03:28 +0100

    fitlers and responsiveness

--- src/Pages/renter_dashboard/renter_dash_comps/NavBarRoute.tsx
+++ src/Pages/renter_dashboard/renter_dash_comps/NavBarRoute.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
-import NavFilters from "./NavFilters";
 import NavBarLinks from "./NavBarLinks";
 
 function NavBarRoute() {
@@ -8,38 +6,7 @@ function NavBarRoute() {
 
 	return (
 		<div className="duration-0">
-			<button
-				className="btn p-2 text-xs bg-purple-500 rounded-md bg-opacity-50"
-				onClick={() => setCurrentScreen(currentScreen ? 0 : 1)}
-			>
-				{!currentScreen ? "Show Filters" : "Hide Filters"}
-			</button>
-			<div className="h-2 border-b mb-2"></div>
-
-			{/* Animate Presence ensures smooth entry/exit animations */}
-			<AnimatePresence mode="wait">
-				{currentScreen ? (
-					<motion.div
-						key="filters"
-						initial={{ opacity: 0, x: 20 }}
-						animate={{ opacity: 1, x: 0 }}
-						exit={{ opacity: 0, x: -20 }}
-						transition={{ duration: 0.3 }}
-					>
-						<NavFilters />
-					</motion.div>
-				) : (
-					<motion.div
-						key="links"
-						initial={{ opacity: 0, x: -20 }}
-						animate={{ opacity: 1, x: 0 }}
-						exit={{ opacity: 0, x: 20 }}
-						transition={{ duration: 0.3 }}
-					>
-						<NavBarLinks />
-					</motion.div>
-				)}
-			</AnimatePresence>
+			<NavBarLinks />
 		</div>
 	);
 }
